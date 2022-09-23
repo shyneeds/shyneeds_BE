c59bf62e173fdc9f86fc90291915c2da91d819d0
limhaekyu <limk9500@gmail.com>
2022-09-23 10:32:29 +0900

    Initial commit

---
+++ src/main/java/com/example/shyneeds_be/ShyneedsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shyneeds_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyneedsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShyneedsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shyneeds_be/ShyneedsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shyneeds_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyneedsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
