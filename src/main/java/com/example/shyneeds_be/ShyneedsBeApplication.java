a9abac85e5f76768fb90fed36702ef37f6dec67c
wonik <twer4774@gmail.com>
2022-09-28 20:51:39 +0900

    setting : Security 기능 끄기(임시), swagger, jpa 설정

--- src/main/java/com/example/shyneeds_be/ShyneedsBeApplication.java
+++ src/main/java/com/example/shyneeds_be/ShyneedsBeApplication.java
@@ -2,8 +2,10 @@ package com.example.shyneeds_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ShyneedsBeApplication {
 
     public static void main(String[] args) {
