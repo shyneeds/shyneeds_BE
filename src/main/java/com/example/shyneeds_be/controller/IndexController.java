ac5a196f9a394fdb68aff08d2a171d392e0ac014
wonik <twer4774@gmail.com>
2022-09-28 21:27:26 +0900

    test : IndexController 스웨거 확인용

--- src/main/java/com/example/shyneeds_be/controller/IndexController.java
+++ src/main/java/com/example/shyneeds_be/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.shyneeds_be.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @ApiOperation(value = "swagger test")
+    @GetMapping("/index")
     public String index(){
         return "index";
     }
