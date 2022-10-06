b6b3e48c366452d15538525cc724d842c420ed29
wonik <twer4774@gmail.com>
2022-10-06 10:34:26 +0900

    setting : CORS 설정

--- src/main/java/com/example/shyneeds_be/global/config/CorsConfig.java
+++ src/main/java/com/example/shyneeds_be/global/config/CorsConfig.java
@@ -5,21 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 내 서버가 응답할할때 josn을 자바스크립테으세 처리할 수 있게 할지 설정
-        config.addAllowedOrigin("*"); // 모든 ip 응답 허용
-        config.addAllowedHeader("*"); // 모든 header 응답 허용
-        config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
-        source.registerCorsConfiguration("/api/**", config);
-
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*", "http://localhost:3000")
+        ;
     }
 }
