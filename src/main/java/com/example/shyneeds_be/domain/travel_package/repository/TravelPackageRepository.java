7a4f1a5139dc42c1a2d9f89d2234b3de57a8b321
wonik <twer4774@gmail.com>
2022-10-11 13:17:01 +0900

    fix : [메인] 카테고리별 상품 리스트 쿼리 및 태그 변경

--- src/main/java/com/example/shyneeds_be/domain/travel_package/repository/TravelPackageRepository.java
+++ src/main/java/com/example/shyneeds_be/domain/travel_package/repository/TravelPackageRepository.java
@@ -5,6 +5,7 @@ import com.example.shyneeds_be.global.model.dto.response.MainTravelPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,7 +25,9 @@ public interface TravelPackageRepository extends JpaRepository<TravelPackage, Lo
     List<TravelPackage> findByMainBanner();
 
     // 카테고리 별 여행 상품 리스트 출력
-    @Query(value = "SELECT CA.title AS categoryTitle, " +
+    @Query(value = "SELECT " +
+            "CA.title AS categoryTitle, " +
+            "SCA.title AS subCategoryTitle, " + // sub category title => tag로 이용
             "TP.id AS id, " +
             "TP.title AS title, " +
             "TP.main_image AS mainImage, " +
@@ -33,10 +36,45 @@ public interface TravelPackageRepository extends JpaRepository<TravelPackage, Lo
             "TP.search_keyword AS keyword " +
             "FROM shyneeds.travel_package AS TP " +
             "INNER JOIN shyneeds.category AS CA " +
-            "ON CA.title LIKE %:searchTitle% " +
-//            "INNER JOIN shyneeds.sub_category AS SCA " +
-//            "ON SCA."
-            "WHERE TP.category_ids IN (CA.id)"
+            "ON CA.title REGEXP :searchTitle " +
+            "INNER JOIN shyneeds.sub_category AS SCA ON SCA.category_id = CA.id " +
+            "WHERE TP.sub_category_ids REGEXP (SCA.id) " +
+            "GROUP BY TP.id " +
+            "ORDER BY TP.updated_at DESC"
             ,nativeQuery = true)
     List<MainTravelPackage> findByCategory(@Param(value = "searchTitle") String category);
+
+
+    // 큐레이션 검색
+    @Query(value = "SELECT " +
+            "SCA.title AS categoryTitle, " + // sub category id -> badge
+            "TP.id AS id, " +
+            "TP.title AS title, " +
+            "TP.main_image AS mainImage, " +
+            "TP.summary AS summary, " +
+            "TP.price AS price, " +
+            "TP.search_keyword AS keyword " +
+            "FROM shyneeds.sub_category AS SCA " +
+            "INNER JOIN shyneeds.travel_package AS TP ON TP.sub_category_ids REGEXP SCA.id " +
+            "INNER JOIN shyneeds.sub_category AS SCA2 ON TP.sub_category_ids REGEXP SCA2.id " +
+            "INNER JOIN shyneeds.sub_category AS SCA3 ON TP.sub_category_ids REGEXP SCA3.id " +
+            "INNER JOIN shyneeds.sub_category AS SCA4 ON TP.sub_category_ids REGEXP SCA4.id " +
+            "INNER JOIN shyneeds.sub_category AS SCA5 ON TP.sub_category_ids REGEXP SCA5.id " +
+            "INNER JOIN shyneeds.sub_category AS SCA6 ON TP.sub_category_ids REGEXP SCA6.id " +
+            "WHERE " +
+            "IF(:ageGroup IS NOT NULL, SCA.title REGEXP :ageGroup, true) " +
+            "AND IF(:accompanyGroup IS NOT NULL, SCA2.title REGEXP :accompanyGroup, true) " +
+            "AND IF(:tendencyGroup IS NOT NULL, SCA3.title REGEXP :tendencyGroup, true) " +
+            "AND IF(:religionGroup IS NOT NULL, SCA4.title REGEXP :religionGroup, true) " +
+            "AND IF(:countryGroup IS NOT NULL, SCA5.title REGEXP :countryGroup, true) " +
+            "AND IF(:themeGroup IS NOT NULL, SCA5.title REGEXP :themeGroup, true) " +
+            "AND TP.disp_flg = 1 AND TP.deleted_flg = 0 " +
+            "GROUP BY TP.id " +
+            "ORDER BY TP.updated_at DESC"
+            ,nativeQuery = true)
+    List<MainTravelPackage> findByCuration(
+            @Param(value = "ageGroup") String ageGroup, @Param(value = "accompanyGroup") String accompanyGroup,
+            @Param(value = "tendencyGroup") String tendencyGroup, @Param(value = "religionGroup") String religionGroup,
+            @Param(value = "countryGroup") String countryGroup, @Param(value = "themeGroup") String themeGroup
+            );
 }
--- src/main/java/com/example/shyneeds_be/global/service/MainService.java
+++ src/main/java/com/example/shyneeds_be/global/service/MainService.java
@@ -1,7 +1,9 @@
 package com.example.shyneeds_be.global.service;
 
+import com.example.shyneeds_be.domain.travel_package.model.dto.response.TravelPackageResponseDto;
 import com.example.shyneeds_be.domain.travel_package.model.entitiy.TravelPackage;
 import com.example.shyneeds_be.domain.travel_package.repository.TravelPackageRepository;
+import com.example.shyneeds_be.global.model.dto.request.CurationRequestDto;
 import com.example.shyneeds_be.global.model.dto.request.MainRequestDto;
 import com.example.shyneeds_be.global.model.dto.response.MainBannerResponseDto;
 import com.example.shyneeds_be.global.model.dto.response.MainTravelPackage;
@@ -49,11 +51,11 @@ public class MainService {
 
             return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "조회에 성공했습니다.", mainResponseDto);
         } catch (Exception e) {
-            return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "조회에 실패했습니다." + e.getMessage());
+            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "조회에 실패했습니다." + e.getMessage());
         }
     }
 
-    // 메인 배너 상품 리스트 출력
+    /*------------------------------ 메인 배너 상품 리스트 출력 -----------------------------------------*/
     private List<MainBannerResponseDto> getMainBanner(){
         try{
 
@@ -75,7 +77,7 @@ public class MainService {
                 .build();
     }
 
-    // 메인 카테고리별 리스트 출력
+    /*------------------------------  메인 카테고리별 리스트 출력 -----------------------------------------*/
     private List<MainTravelPackageResponseDto> getTravelPackageList(String category){
         try{
             return travelPackageRepository.findByCategory(category).stream().map(this::responseMainTravelResponseDto).toList();
@@ -88,14 +90,47 @@ public class MainService {
     private MainTravelPackageResponseDto responseMainTravelResponseDto(MainTravelPackage mainTravelPackage){
         String imageDir = "https://shyneeds.s3.ap-northeast-2.amazonaws.com/package/"+
                 mainTravelPackage.getTitle()+"/main/"+mainTravelPackage.getMainImage();
+        String tagUrl = "https://shyneeds.s3.ap-northeast-2.amazonaws.com/design/badge/" + mainTravelPackage.getSubCategoryTitle() +".svg";
         return MainTravelPackageResponseDto.builder()
                 .id(mainTravelPackage.getId())
                 .title(mainTravelPackage.getTitle())
                 .imageUrl(imageDir)
                 .price(mainTravelPackage.getPrice())
                 .summary(mainTravelPackage.getSummary())
-                .tag(mainTravelPackage.getCategoryTitle())
+                .tag((mainTravelPackage.getCategoryTitle().contains("지역별상품") ?  null : tagUrl))
                 .keyword(mainTravelPackage.getKeyword())
                 .build();
+    }
+
+
+    /*------------------------------ 큐레이션 결과 -----------------------------------------*/
+    public ApiResponseDto<List<MainTravelPackageResponseDto>> getResultCuration(CurationRequestDto curationRequestDto) {
+        try {
+
+            List<MainTravelPackageResponseDto> curationResultPackageList = new ArrayList<>();
+            String age = curationRequestDto.getAge();
+            String accompany = curationRequestDto.getAge();
+            String tendency = curationRequestDto.getTendency();
+            String religion = curationRequestDto.getReligion();
+            String country = curationRequestDto.getCountry();
+            String theme = curationRequestDto.getTheme();
+            if(age != null || accompany != null ||  tendency != null || religion != null
+                    || country != null || theme != null
+            ){
+                curationResultPackageList = travelPackageRepository.findByCuration(
+                        age,
+                        accompany,
+                        tendency,
+                        religion,
+                        country,
+                        theme).stream().map(this::responseMainTravelResponseDto).toList();
+            }
+
+
+            return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "조회에 성공했습니다.", curationResultPackageList);
+        } catch (Exception e) {
+            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "조회에 실패했습니다." + e.getMessage());
+
+        }
     }
 }
