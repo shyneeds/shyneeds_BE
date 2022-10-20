0f973b79f7575206ea8de77b90c8fabc212bc55f
wonik <twer4774@gmail.com>
2022-10-20 15:15:04 +0900

    feat : [Community] Review 리스트 호출 API 개발 (검색, 페이지네이션 포함)

---
+++ src/main/java/com/example/shyneeds_be/domain/community/controller/ReviewController.java
@@ -0,0 +1,30 @@
+package com.example.shyneeds_be.domain.community.controller;
+
+import com.example.shyneeds_be.domain.community.model.dto.response.ReviewMainResponseDto;
+import com.example.shyneeds_be.domain.community.service.ReviewService;
+import com.example.shyneeds_be.global.network.response.ApiResponseDto;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/review")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @ApiOperation(value = "리뷰 리스트", notes = "[검색] search  = all or title")
+    @GetMapping("")
+    public ApiResponseDto<List<ReviewMainResponseDto>> getReviewList(@PathParam("search") String search, @PageableDefault(value = 12)Pageable pageable){
+        return reviewService.getReviewList(search, pageable);
+    }
+
+}
