382392d93e6472dd32f5d257b7e8d83eaa7781d6
wonik <twer4774@gmail.com>
2022-10-25 12:54:19 +0900

    feat : [Main] 카테고리 타이틀 리스트 API 추가

---
+++ src/main/java/com/example/shyneeds_be/domain/category/model/entity/CategoryTitle.java
@@ -0,0 +1,9 @@
+package com.example.shyneeds_be.domain.category.model.entity;
+
+/**
+ * 메인 화면 큐레이션에 노출
+ */
+public interface CategoryTitle {
+    String getTitle();
+    String getSubCategoryTitle();
+}
--- src/main/java/com/example/shyneeds_be/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/example/shyneeds_be/domain/category/repository/CategoryRepository.java
@@ -1,7 +1,18 @@
 package com.example.shyneeds_be.domain.category.repository;
 
 import com.example.shyneeds_be.domain.category.model.entity.Category;
+import com.example.shyneeds_be.domain.category.model.entity.CategoryTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+    @Query(value = "SELECT CA.title AS title, SCA.title AS subCategoryTitle FROM shyneeds.category AS CA " +
+            "INNER JOIN shyneeds.sub_category AS SCA ON CA.id = SCA.category_id " +
+            "WHERE CA.disp_flg = 1 AND SCA.disp_flg = 1"
+    ,nativeQuery = true)
+    List<CategoryTitle> getCategoryTitleList();
 }
